cb58a0e0818d66e18236a3ed77f8df1bb09dc6ea
Michael Smith <mlsmith45@gmail.com>
2015-05-13 14:03:41 -0400

    Bribe with 'and' boolean
    Adds bribe and currency to container variable

--- stoppedByGuards.js
+++ stoppedByGuards.js
@@ -5,22 +5,45 @@ switch(user) {
         var smuggle = prompt("Good choice! Once you get to the market, you'll be close to the Castle. Should you try and smuggle an item to help you? ").toUpperCase();
         if (smuggle === 'YES') {
           var item = prompt("You can smuggle one item to help you. Make your choice: Knife or Money").toUpperCase();
-            if (item === 'KNIFE' || item === 'MONEY'){
-              console.log("The Guards don't notice, and let you pass with the item.");
+            if (item === 'KNIFE') {
+                  console.log("The Guards don't notice, and let you pass with the item.");
             }
-             else {
+            else if (item === 'MONEY') {
+                var container = prompt("Do you hide the Money in your Pocket or Pouch?").toUpperCase();
+                
+                if (container === 'POCKET') {
+                    console.log("The Guards check your money pouch. Since it's empty, they let you through.");
+                }
+
+                else if (container === 'POUCH') {
+                    var bribe = prompt("The guards find your money. They demand a bribe to let you continue. If you don't bribe them, you'll have to go home. Do you Bribe them? Yes or No?").toUpperCase();
+                    var currency = prompt ("They'll only take money in the King's Currency. Do you have any money in the King's Currency? Yes or No?").toUpperCase();
+                    if (bribe === 'YES' && currency === 'YES') {
+                        console.log("The guards let you continue to the market.");
+                    } 
+
+                    else {
+                        console.log("You don't have enough money to bribe them. You must go back home."); 
+                    }     
+                }
+            }
+
+            else {
                 console.log("Be sure to choose to smuggle either a Knife or Money");        
-             }
-        } // end 1st if
-          else { console.log("The Guards let you pass, but you'll be at a dissadvantage when you reach the market.");
+            }
+
+        } // end smuggle if
+          
+          else { console.log("The Guards let you pass, but you'll be at a dissadvantage when you reach the market."); // smuggle if
           }
-    break;
+    break; // end Market Case
 
 
      case 'CASTLE':
         var fight = prompt("The guards try and arrest you. You must Fight or Run.").toUpperCase();
         
         if (fight === 'FIGHT') {
+            var weapon = prompt("Choose your weapon: Sword or Axe.");
             if (weapon === 'SWORD' || weapon === 'AXE') {
                 var result = Math.floor(Math.random() * 2);
                     if (result) {
